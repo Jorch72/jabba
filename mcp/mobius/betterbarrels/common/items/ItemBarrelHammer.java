e1e24fd2ef48a9fd68dccc593281611167837348
taelnia <shotchkiss+bitbucket@gmail.com>
2014-01-31 16:04:51 -0500

    forgot to set subtype flag

--- mcp/mobius/betterbarrels/common/items/ItemBarrelHammer.java
+++ mcp/mobius/betterbarrels/common/items/ItemBarrelHammer.java
@@ -47,7 +47,8 @@ public class ItemBarrelHammer extends Item implements IOverlayItem{
    
    public ItemBarrelHammer(int id){
       super(id);
-        this.setMaxStackSize(1);    
+        this.setMaxStackSize(1);
+        this.setHasSubtypes(true);
         this.setUnlocalizedName("hammer");
    }
    
