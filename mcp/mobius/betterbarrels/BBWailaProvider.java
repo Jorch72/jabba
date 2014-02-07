5bd47d4e3afd11db45a95a7a67a9ca4261bc86f7
taelnia <shotchkiss+bitbucket@gmail.com>
2014-02-07 13:41:13 -0500

    WAILA tooltip lacalization

--- mcp/mobius/betterbarrels/BBWailaProvider.java
+++ mcp/mobius/betterbarrels/BBWailaProvider.java
@@ -3,6 +3,7 @@ package mcp.mobius.betterbarrels;
 import java.util.List;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import mcp.mobius.betterbarrels.common.blocks.TileEntityBarrel;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
@@ -23,21 +24,21 @@ public class BBWailaProvider implements IWailaDataProvider {
 		TileEntityBarrel tebarrel = (TileEntityBarrel)accessor.getTileEntity();
 		ItemStack barrelStack = tebarrel.getStorage().getItem();
 		
-		currenttip.add(String.format("Structural level : %d", tebarrel.coreUpgrades.levelStructural));
-		currenttip.add(String.format("Upgrade slots : %d / %d", tebarrel.coreUpgrades.getFreeSlots(), tebarrel.coreUpgrades.getMaxUpgradeSlots()));		
+		currenttip.add(StatCollector.translateToLocalFormatted("text.jabba.waila.structural", tebarrel.coreUpgrades.levelStructural));
+		currenttip.add(StatCollector.translateToLocalFormatted("text.jabba.waila.upgrades", tebarrel.coreUpgrades.getFreeSlots(), tebarrel.coreUpgrades.getMaxUpgradeSlots()));		
 		
 		if (barrelStack != null){
 			if(config.getConfig("bb.itemtype"))
 				currenttip.add(barrelStack.getDisplayName());
 			if(config.getConfig("bb.itemnumb"))
-					currenttip.add(String.format("%d / %d items", tebarrel.getStorage().getAmount(), tebarrel.getStorage().getItem().getMaxStackSize() * tebarrel.getStorage().getMaxStacks()));
+					currenttip.add(StatCollector.translateToLocalFormatted("text.jabba.waila.items", tebarrel.getStorage().getAmount(), tebarrel.getStorage().getItem().getMaxStackSize() * tebarrel.getStorage().getMaxStacks()));
 			if(config.getConfig("bb.space"))
-					currenttip.add(String.format("%d stacks max", tebarrel.getStorage().getMaxStacks()));			
+					currenttip.add(StatCollector.translateToLocalFormatted("text.jabba.waila.stacks", tebarrel.getStorage().getMaxStacks()));			
 		} else {
 			if(config.getConfig("bb.itemtype"))
-				currenttip.add("<Empty>");
+				currenttip.add(StatCollector.translateToLocal("text.jabba.waila.empty"));
 			if(config.getConfig("bb.space"))
-				currenttip.add(String.format("%d stacks max", tebarrel.getStorage().getMaxStacks()));			
+				currenttip.add(StatCollector.translateToLocalFormatted("text.jabba.waila.stacks", tebarrel.getStorage().getMaxStacks()));			
 		}
 		
 		return currenttip;
@@ -49,9 +50,9 @@ public class BBWailaProvider implements IWailaDataProvider {
 	}	
 	
 	public static void callbackRegister(IWailaRegistrar registrar){
-		registrar.addConfig("Better barrels", "bb.itemtype", "Barrel content");
-		registrar.addConfig("Better barrels", "bb.itemnumb", "Items quantity");
-		registrar.addConfig("Better barrels", "bb.space", "Max stacks");
+		registrar.addConfig(StatCollector.translateToLocal("itemGroup.jabba"), "bb.itemtype", StatCollector.translateToLocal("text.jabba.waila.key.content"));
+		registrar.addConfig(StatCollector.translateToLocal("itemGroup.jabba"), "bb.itemnumb", StatCollector.translateToLocal("text.jabba.waila.key.quantity"));
+		registrar.addConfig(StatCollector.translateToLocal("itemGroup.jabba"), "bb.space", StatCollector.translateToLocal("text.jabba.waila.key.stacks"));
 		registrar.registerBodyProvider(new BBWailaProvider(), TileEntityBarrel.class);
 	}
 	
