b1984f8e3b6e78b724150336d26fc2b363f26645
ProfMobius <ProfMobius@localhost>
2014-03-13 19:43:29 +0100

    Version bump

--- mcp/mobius/betterbarrels/BetterBarrels.java
+++ mcp/mobius/betterbarrels/BetterBarrels.java
@@ -39,7 +39,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid=BetterBarrels.modid, name="JABBA", version="1.1.2_dev1", dependencies="after:Waila;after:NotEnoughItems")
+@Mod(modid=BetterBarrels.modid, name="JABBA", version="1.1.2", dependencies="after:Waila;after:NotEnoughItems")
 @NetworkMod(channels = {"JABBA"}, clientSideRequired=true, serverSideRequired=false, packetHandler=BarrelPacketHandler.class)
 
 public class BetterBarrels {
