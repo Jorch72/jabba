dffafd477c9032add5e174c5b97987559c5d5f23
taelnia <shotchkiss+bitbucket@gmail.com>
2014-04-17 19:50:50 -0400

    -Better config support for materials list and upgradeItem:     Will accept the following formats(yes the quotes are needed):         Ore.oreDictEntry         "<ItemID[:OptionalMeta]>" - Move the recipe generation for structural tiers to postInit to allow for mods that create/register items during init

--- mcp/mobius/betterbarrels/RecipeHandler.java
+++ mcp/mobius/betterbarrels/RecipeHandler.java
@@ -33,16 +33,6 @@ public class RecipeHandler {
 	private ItemStack upgradeItem = null;
 	
 	public void registerRecipes(){
-		try {
-			upgradeItem = new ItemStack(Item.itemsList[BetterBarrels.upgradeItemID]);
-		} catch (Throwable t) {
-			BetterBarrels.log.severe("Requested item with id " + BetterBarrels.upgradeItemID + " for tier upgrade recipes was not found, using the default of vanilla fence");
-			upgradeItem = new ItemStack(Block.fence);
-		}
-
-		for (int i = 0; i < Math.min(StructuralLevel.LEVELS.length-1, StructuralLevel.maxCraftableTier); i++)
-			this.addStructuralUpgrade(i, StructuralLevel.upgradeMaterialsList[i]);
-		
 		this.addCoreUpgrade(0, BetterBarrels.blockBarrel);
 		this.addCoreUpgrade(1, "transdimBlock");
 		this.addCoreUpgrade(2, Block.blockRedstone);
@@ -97,6 +87,19 @@ public class RecipeHandler {
 		}
 	}
 
+	public void registerLateRecipes() {
+		try {
+			Utils.Material mat = new Utils.Material(BetterBarrels.upgradeItemStr);
+			upgradeItem = mat.getStack();
+		} catch (Throwable t) {
+			BetterBarrels.log.severe("Requested item with id " + BetterBarrels.upgradeItemStr + " for tier upgrade recipes was not found, using the default of vanilla fence");
+			upgradeItem = new ItemStack(Block.fence);
+		}
+
+		for (int i = 0; i < Math.min(StructuralLevel.LEVELS.length-1, StructuralLevel.maxCraftableTier); i++)
+			this.addStructuralUpgrade(i, StructuralLevel.upgradeMaterialsList[i]);
+	}
+	
 	private void addCoreUpgradeUpgrade(int resultMeta, int sourceMeta) {
       GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BetterBarrels.itemUpgradeCore, 1, resultMeta),
                                                     new Object[]{new ItemStack(BetterBarrels.itemUpgradeCore, 1, sourceMeta),
@@ -109,49 +112,26 @@ public class RecipeHandler {
 	}
 	
 	private void addStructuralUpgrade(int level, String variableComponent){
-		String type     = variableComponent.split("\\.")[0];
-		String compoStr = variableComponent.split("\\.")[1];
-		this.addStructuralUpgrade_(level, compoStr);
-		
-		/*
-		if (type.equals("Ore"))
-			this.addStructuralUpgrade_(level, compoStr);
+		Utils.Material mat = new Utils.Material(variableComponent);
 
-		if (type.equals("Block")){
+		if (mat.oreName != null) {
+			this.addStructuralUpgrade_(level, mat.oreName);
+		} else {
 			try {
-				Block compo = (Block)Class.forName("net.minecraft.block.Block").getField(compoStr).get(null);
-				this.addStructuralUpgrade_(level, compo);
-			} catch (Exception e) {
+				this.addStructuralUpgrade_(level, mat.getStack());
+			} catch (Throwable t) {
 				BetterBarrels.log.severe("Error while trying to register recipe with material " + variableComponent);
 			}
 		}
-
-		if (type.equals("Item")){
-			try {
-				Item compo = (Item)Class.forName("net.minecraft.item.Item").getField(compoStr).get(null);
-				this.addStructuralUpgrade_(level, compo);
-			} catch (Exception e) {
-				BetterBarrels.log.severe("Error while trying to register recipe with material " + variableComponent);
-			}
-		}
-		*/		
-		
 	}
 	
-	private void addStructuralUpgrade_(int level, Item variableComponent){
+	private void addStructuralUpgrade_(int level, ItemStack variableComponent){
 		GameRegistry.addRecipe(new ItemStack(BetterBarrels.itemUpgradeStructural,1,level), new Object[] 
 				{"PBP", "B B", "PBP",
 				'P', upgradeItem, 
 				'B', variableComponent});		
 	}
 
-	private void addStructuralUpgrade_(int level, Block variableComponent){
-		GameRegistry.addRecipe(new ItemStack(BetterBarrels.itemUpgradeStructural,1,level), new Object[] 
-				{"PBP", "B B", "PBP",
-				'P', upgradeItem, 
-				'B', new ItemStack(variableComponent,1)});		
-	}	
-	
 	private void addStructuralUpgrade_(int level, String variableComponent){
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BetterBarrels.itemUpgradeStructural,1,level), new Object[] 
 				{"PBP", "B B", "PBP",
--- mcp/mobius/betterbarrels/Utils.java
+++ mcp/mobius/betterbarrels/Utils.java
@@ -1,11 +1,16 @@
 package mcp.mobius.betterbarrels;
 
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class Utils {
 
@@ -56,4 +61,57 @@ public class Utils {
       source.worldObj.spawnEntityInWorld(droppedEntity);
    }
 
+	public static class Material {
+		public String oreName;
+		public int id;
+		public int meta;
+
+		public Material(String in) {
+			if (in.contains("Ore.")) {
+				oreName = in.split("\\.")[1];
+			} else if (in.contains("<")) {
+				String itemStr = in.substring(in.indexOf('<') + 1, in.indexOf('>'));
+
+				int metaCh = itemStr.indexOf(':');
+				int wildCh = itemStr.indexOf('*');
+
+				if (metaCh >= 0) {
+					if (wildCh == metaCh + 1) {
+						meta = OreDictionary.WILDCARD_VALUE;
+					} else {
+						meta = Integer.parseInt(itemStr.substring(metaCh + 1, itemStr.length()));
+					}
+
+					id = Integer.parseInt(itemStr.substring(0, metaCh));
+				} else {
+					id = Integer.parseInt(itemStr);
+					meta = 0;
+				}
+			} else {
+				BetterBarrels.log.severe("Unable to parse input string into oreDict or item:" + in);
+			}
+		}
+
+		public ItemStack getStack() {
+			ItemStack ret = null;
+			if (this.oreName != null) {
+				ArrayList<ItemStack> ores = OreDictionary.getOres(this.oreName);
+
+				if (ores.size() > 0) {
+					ret = ores.get(0);
+				} else {
+					ret = new ItemStack(Block.portal);
+				}
+				BetterBarrels.debug("05 - Looking up [" + this.oreName + "] and found: " + ret.getDisplayName());
+			} else {
+				try {
+					ret = new ItemStack(Item.itemsList[this.id], 1, this.meta);
+					BetterBarrels.debug("05 - Looking up [" + (this.id + ":" + this.meta) + "] and found: " + ret.getDisplayName());
+				} catch (Throwable t) {
+					BetterBarrels.log.severe("Error while trying to initialize material with ID number " + (this.id + ":" + this.meta));
+				}
+			}
+			return ret;
+		}
+	}
 }
