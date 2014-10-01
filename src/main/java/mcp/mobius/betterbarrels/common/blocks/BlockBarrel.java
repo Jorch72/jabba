27389822b9653ddf8be288e61cbeb77e0f801d78
taelnia <shotchkiss+bitbucket@gmail.com>
2014-10-01 15:21:07 -0400

    fix inconsistent indentation, will check rest of source later...

--- src/main/java/mcp/mobius/betterbarrels/common/blocks/BlockBarrel.java
+++ src/main/java/mcp/mobius/betterbarrels/common/blocks/BlockBarrel.java
@@ -34,28 +34,28 @@ public class BlockBarrel extends BlockContainer{
 	public static IIcon   text_lock       = null;
 	public static IIcon   text_linked     = null;
 	public static IIcon   text_locklinked = null;
-	
-    public BlockBarrel(){
-    	super(new Material(MapColor.woodColor) {
-    		{
-    			this.setBurning();
-    			this.setAdventureModeExempt();
-    		}
-    	});
-        this.setHardness(2.0F);
-        this.setResistance(5.0F);
-        this.setHarvestLevel("axe", 1);
-        this.setBlockName("blockbarrel");
+
+	public BlockBarrel(){
+		super(new Material(MapColor.woodColor) {
+			{
+				this.setBurning();
+				this.setAdventureModeExempt();
+			}
+		});
+		this.setHardness(2.0F);
+		this.setResistance(5.0F);
+		this.setHarvestLevel("axe", 1);
+		this.setBlockName("blockbarrel");
 		this.setCreativeTab(JabbaCreativeTab.tab);
-    }
+	}
 
 	@Override
 	public TileEntity createNewTileEntity(World world, int i) {
-        return new TileEntityBarrel();
+		return new TileEntityBarrel();
 	}
-	
-    @Override    
-    public void registerBlockIcons(IIconRegister iconRegister){
+
+	@Override    
+	public void registerBlockIcons(IIconRegister iconRegister){
 		BlockBarrel.text_sidehopper  = iconRegister.registerIcon(BetterBarrels.modid + ":" + "facade_hopper");
 		BlockBarrel.text_siders      = iconRegister.registerIcon(BetterBarrels.modid + ":" + "facade_redstone");
 		BlockBarrel.text_lock        = iconRegister.registerIcon(BetterBarrels.modid + ":" + "overlay_locked");
@@ -64,155 +64,155 @@ public class BlockBarrel extends BlockContainer{
 		if (StructuralLevel.LEVELS == null) StructuralLevel.createLevelArray();
 		for (int i = 0; i < StructuralLevel.LEVELS.length; i++)
 			StructuralLevel.LEVELS[i].registerBlockIcons(iconRegister, i);
-    }
-
-    @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack par6ItemStack){
-    	// We get the orientation and check if the TE is already properly created.
-    	// If so we set the entity value to the correct orientation and set the block meta to 1 to kill the normal block rendering.
-    	
-        int barrelOrientation = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
-        TileEntityBarrel barrelEntity = (TileEntityBarrel)world.getTileEntity(x, y, z);
-        
-        if (barrelEntity != null){
-	        switch (barrelOrientation){
-	        	case 0:
-	        		barrelEntity.orientation = ForgeDirection.NORTH;
-	        		break;
-	        	case 1:
-	        		barrelEntity.orientation = ForgeDirection.EAST;	        		
-	        		break;
-	        	case 2:
-	        		barrelEntity.orientation = ForgeDirection.SOUTH;	        		
-	        		break;
-	        	case 3:
-	        		barrelEntity.orientation = ForgeDirection.WEST;	        		
-	        		break;        		
-	        }
-	        barrelEntity.sideUpgrades[barrelEntity.orientation.ordinal()] = UpgradeSide.FRONT;
-        }
-    }
-    
-    @Override
-    public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z) {
-    	return this.removedByPlayer(world, player, x, y, z, false);
-    }
-
-    @Override
-    public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z, boolean willHarvest) {
-    	if (player.capabilities.isCreativeMode && !player.isSneaking() ) {
-    		this.onBlockClicked(world, x, y, z, player);
-    		return false;
-    	} else {
-    		return world.setBlockToAir(x, y, z);
-    	}
-    }
-
-    @Override
-    public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player){
-        if (!world.isRemote){
-            TileEntity tileEntity = world.getTileEntity(x, y, z);
-            ((TileEntityBarrel)tileEntity).leftClick(player);
-        }
-    }     
-    
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float var7, float var8, float var9){
-        if (!world.isRemote){
-            TileEntity tileEntity = world.getTileEntity(x, y, z);
-            ((TileEntityBarrel)tileEntity).rightClick(player, side);        	
-        }
-        return true;
-    }
-    
-	private void dropStack(World world, ItemStack stack, int x, int y, int z){
-    	Random random = new Random();
-        float var10 = random.nextFloat() * 0.8F + 0.1F;
-        float var11 = random.nextFloat() * 0.8F + 0.1F;
-        EntityItem items;
-
-        
-        for (float var12 = random.nextFloat() * 0.8F + 0.1F; stack.stackSize > 0; world.spawnEntityInWorld(items))
-        {
-            int var13 = random.nextInt(21) + 10;
-
-            if (var13 > stack.stackSize)
-            {
-                var13 = stack.stackSize;
-            }
-
-            stack.stackSize -= var13;
-            items = new EntityItem(world, x + var10, y + var11, z + var12, new ItemStack(stack.getItem(), var13, stack.getItemDamage()));
-            float var15 = 0.05F;
-            items.motionX = (float)random.nextGaussian() * var15;
-            items.motionY = (float)random.nextGaussian() * var15 + 0.2F;
-            items.motionZ = (float)random.nextGaussian() * var15;
-
-            if (stack.hasTagCompound())
-            {
-            	items.getEntityItem().setTagCompound((NBTTagCompound)stack.getTagCompound().copy());
-            }
-        }  		
 	}
-	
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack par6ItemStack){
+		// We get the orientation and check if the TE is already properly created.
+		// If so we set the entity value to the correct orientation and set the block meta to 1 to kill the normal block rendering.
+
+		int barrelOrientation = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+		TileEntityBarrel barrelEntity = (TileEntityBarrel)world.getTileEntity(x, y, z);
+
+		if (barrelEntity != null){
+			switch (barrelOrientation){
+			case 0:
+				barrelEntity.orientation = ForgeDirection.NORTH;
+				break;
+			case 1:
+				barrelEntity.orientation = ForgeDirection.EAST;	        		
+				break;
+			case 2:
+				barrelEntity.orientation = ForgeDirection.SOUTH;	        		
+				break;
+			case 3:
+				barrelEntity.orientation = ForgeDirection.WEST;	        		
+				break;        		
+			}
+			barrelEntity.sideUpgrades[barrelEntity.orientation.ordinal()] = UpgradeSide.FRONT;
+		}
+	}
+
+	@Override
+	public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z) {
+		return this.removedByPlayer(world, player, x, y, z, false);
+	}
+
+	@Override
+	public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z, boolean willHarvest) {
+		if (player.capabilities.isCreativeMode && !player.isSneaking() ) {
+			this.onBlockClicked(world, x, y, z, player);
+			return false;
+		} else {
+			return world.setBlockToAir(x, y, z);
+		}
+	}
+
+	@Override
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player){
+		if (!world.isRemote){
+			TileEntity tileEntity = world.getTileEntity(x, y, z);
+			((TileEntityBarrel)tileEntity).leftClick(player);
+		}
+	}     
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float var7, float var8, float var9){
+		if (!world.isRemote){
+			TileEntity tileEntity = world.getTileEntity(x, y, z);
+			((TileEntityBarrel)tileEntity).rightClick(player, side);        	
+		}
+		return true;
+	}
+
+	private void dropStack(World world, ItemStack stack, int x, int y, int z){
+		Random random = new Random();
+		float var10 = random.nextFloat() * 0.8F + 0.1F;
+		float var11 = random.nextFloat() * 0.8F + 0.1F;
+		EntityItem items;
+
+
+		for (float var12 = random.nextFloat() * 0.8F + 0.1F; stack.stackSize > 0; world.spawnEntityInWorld(items))
+		{
+			int var13 = random.nextInt(21) + 10;
+
+			if (var13 > stack.stackSize)
+			{
+				var13 = stack.stackSize;
+			}
+
+			stack.stackSize -= var13;
+			items = new EntityItem(world, x + var10, y + var11, z + var12, new ItemStack(stack.getItem(), var13, stack.getItemDamage()));
+			float var15 = 0.05F;
+			items.motionX = (float)random.nextGaussian() * var15;
+			items.motionY = (float)random.nextGaussian() * var15 + 0.2F;
+			items.motionZ = (float)random.nextGaussian() * var15;
+
+			if (stack.hasTagCompound())
+			{
+				items.getEntityItem().setTagCompound((NBTTagCompound)stack.getTagCompound().copy());
+			}
+		}  		
+	}
+
 	@Override
 	public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int par5) {
 		if (world.isRemote) return;
-		
+
 		TileEntityBarrel barrelEntity = (TileEntityBarrel)world.getTileEntity(x, y, z);		
 
 		// We drop the structural upgrades
 		if ((barrelEntity != null) && (barrelEntity.coreUpgrades.levelStructural > 0)){
-    		int currentUpgrade = barrelEntity.coreUpgrades.levelStructural;
-    		while (currentUpgrade > 0){
-    			ItemStack droppedStack = new ItemStack(BetterBarrels.itemUpgradeStructural, 1, currentUpgrade-1);
-    			this.dropStack(world, droppedStack, x, y, z);
-    			currentUpgrade -= 1;
-    		}
-    	}    	
-    	
-    	// We drop the core upgrades
-    	if (barrelEntity != null){
-    		for (UpgradeCore core : barrelEntity.coreUpgrades.upgradeList){
+			int currentUpgrade = barrelEntity.coreUpgrades.levelStructural;
+			while (currentUpgrade > 0){
+				ItemStack droppedStack = new ItemStack(BetterBarrels.itemUpgradeStructural, 1, currentUpgrade-1);
+				this.dropStack(world, droppedStack, x, y, z);
+				currentUpgrade -= 1;
+			}
+		}    	
+
+		// We drop the core upgrades
+		if (barrelEntity != null){
+			for (UpgradeCore core : barrelEntity.coreUpgrades.upgradeList){
 				ItemStack droppedStack = new ItemStack(BetterBarrels.itemUpgradeCore, 1, core.ordinal());
- 				this.dropStack(world, droppedStack, x, y, z);
-    		}
-    	}     	
-    	
-    	// We drop the side upgrades
-    	if (barrelEntity != null){
-    		for (int i = 0; i < 6; i++){
-    			Item upgrade = UpgradeSide.mapItem[barrelEntity.sideUpgrades[i]];
-    			if (upgrade != null){
-    				ItemStack droppedStack = new ItemStack(upgrade, 1, UpgradeSide.mapMeta[barrelEntity.sideUpgrades[i]]);
-    				this.dropStack(world, droppedStack, x, y, z);
-    			}
-    		}
-    	}    	
-    	
-    	// We drop the stacks
-        if ((barrelEntity != null) && (barrelEntity.getStorage().hasItem()) && (!barrelEntity.getLinked()))
-        {
-        	barrelEntity.updateEntity();
-        	int ndroppedstacks = 0;
-        	ItemStack droppedstack = barrelEntity.getStorage().getStack();
-        	// TODO : is this just an amount limiter to prevent too many items spawning into the world?
-        	// limits max number of dropped stacks to 64, perhaps should be limited to 64 * storage upgrade count?
-        	while ((droppedstack != null) && (ndroppedstacks <= 64)){ //TODO: shouldn't this be the max stack size of the barrel, not 64?
-        		ndroppedstacks += 1;
-        		
-        		if (droppedstack != null)
-        			this.dropStack(world, droppedstack, x, y, z);
+				this.dropStack(world, droppedStack, x, y, z);
+			}
+		}     	
 
-        		droppedstack    = barrelEntity.getStorage().getStack();
-        	}
-        }		
+		// We drop the side upgrades
+		if (barrelEntity != null){
+			for (int i = 0; i < 6; i++){
+				Item upgrade = UpgradeSide.mapItem[barrelEntity.sideUpgrades[i]];
+				if (upgrade != null){
+					ItemStack droppedStack = new ItemStack(upgrade, 1, UpgradeSide.mapMeta[barrelEntity.sideUpgrades[i]]);
+					this.dropStack(world, droppedStack, x, y, z);
+				}
+			}
+		}    	
 
-        try{
-        	BSpaceStorageHandler.instance().unregisterEnderBarrel(barrelEntity.id);
-        } catch (Exception e){
-        	BetterBarrels.log.info("Tried to remove the barrel from the index without a valid entity");
-        }
+		// We drop the stacks
+		if ((barrelEntity != null) && (barrelEntity.getStorage().hasItem()) && (!barrelEntity.getLinked()))
+		{
+			barrelEntity.updateEntity();
+			int ndroppedstacks = 0;
+			ItemStack droppedstack = barrelEntity.getStorage().getStack();
+			// TODO : is this just an amount limiter to prevent too many items spawning into the world?
+			// limits max number of dropped stacks to 64, perhaps should be limited to 64 * storage upgrade count?
+			while ((droppedstack != null) && (ndroppedstacks <= 64)){ //TODO: shouldn't this be the max stack size of the barrel, not 64?
+				ndroppedstacks += 1;
+
+				if (droppedstack != null)
+					this.dropStack(world, droppedstack, x, y, z);
+
+				droppedstack    = barrelEntity.getStorage().getStack();
+			}
+		}		
+
+		try{
+			BSpaceStorageHandler.instance().unregisterEnderBarrel(barrelEntity.id);
+		} catch (Exception e){
+			BetterBarrels.log.info("Tried to remove the barrel from the index without a valid entity");
+		}
 	}
 
 	@Override
@@ -228,18 +228,18 @@ public class BlockBarrel extends BlockContainer{
 	@Override
 	public int isProvidingStrongPower(IBlockAccess world, int x, int y, int z, int side){
 		return this.isProvidingWeakPower(world, x, y, z, side);
-    }
+	}
 
-    @Override
+	@Override
 	public boolean canProvidePower(){
-    	return true;
-    }
+		return true;
+	}
 
-    @Override
+	@Override
 	public int isProvidingWeakPower(IBlockAccess world, int x, int y, int z, int side){
-    	TileEntityBarrel barrel = (TileEntityBarrel)world.getTileEntity(x, y, z);
-    	return barrel.getRedstonePower(side);
-    }
+		TileEntityBarrel barrel = (TileEntityBarrel)world.getTileEntity(x, y, z);
+		return barrel.getRedstonePower(side);
+	}
 
 	@Override
 	public int getRenderType() {
@@ -311,19 +311,19 @@ public class BlockBarrel extends BlockContainer{
 
 		return false;
 	}
-    
-    /*
+
+	/*
     @Override
     public boolean isBlockNormalCube(World world, int x, int y, int z){
     	return false;
     }
-    */
-    
-    /*
+	 */
+
+	/*
     private boolean isIndirectlyPowered(World par1World, int par2, int par3, int par4)
     {
         int l = par1World.getBlockMetadata(par2, par3, par4);
         return l == 5 && par1World.getIndirectPowerOutput(par2, par3 - 1, par4, 0) ? true : (l == 3 && par1World.getIndirectPowerOutput(par2, par3, par4 - 1, 2) ? true : (l == 4 && par1World.getIndirectPowerOutput(par2, par3, par4 + 1, 3) ? true : (l == 1 && par1World.getIndirectPowerOutput(par2 - 1, par3, par4, 4) ? true : l == 2 && par1World.getIndirectPowerOutput(par2 + 1, par3, par4, 5))));
     }
-    */    
+	 */    
 }
