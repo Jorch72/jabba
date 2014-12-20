a170f4c4daffd7fa2e26310ba48f835e87c07bee
taelnia <shotchkiss+bitbucket@gmail.com>
2014-12-20 10:43:58 -0500

    updated for latest indev waila

--- src/main/java/mcp/mobius/betterbarrels/BBWailaProvider.java
+++ src/main/java/mcp/mobius/betterbarrels/BBWailaProvider.java
@@ -7,6 +7,7 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -60,7 +61,7 @@ public class BBWailaProvider implements IWailaDataProvider {
 	}
 
 	@Override
-	public NBTTagCompound getNBTData(TileEntity te, NBTTagCompound tag, World world, int x, int y, int z) {
+	public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x, int y, int z) {
 		return tag;
 	}
 }
